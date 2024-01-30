fe3915dd526549d8aeef39c762fcb537b04e5678
Billy <billy.xu99@gmail.com>
2024-01-30 20:29:17 +1000

    Added score to cards and the respective CSS

--- src/Cards.jsx
+++ src/Cards.jsx
@@ -1,5 +1,10 @@
-// importing components from React-bootstrap import React, { useState, useEffect } from 'react'
+
 // TO DO LIST: 
+// - Add more information to each card (producers, rank, rating, score)
+// - Add another component for loading more animeTitles, then do logic for loading that data
+// - Add a view more component onClick of button 
+// - Complete logic for the search feature
+
 import Button from 'react-bootstrap/Button'
 import Card from 'react-bootstrap/Card'
 import { useState, useEffect } from 'react'
@@ -13,12 +18,13 @@ const Cards = () => {
     const [animeData, setAnimeData] = useState([])
     const [url, setUrl] = useState('https://api.jikan.moe/v4/anime?q=&sfw')
   
+// fetch data from api using axios, set the data to animeData state, then log that data
     const getAnimeData = async () => {
         await axios
         .get(url)
         .then(response => {
             setAnimeData(response.data.data)
-            console.log(animeData)
+            console.log('response (data)', response)
         })
     }
   
@@ -29,43 +35,36 @@ const Cards = () => {
                                     
     return (
             <>
-                {/* {data.map((item) => ( */}
-                    {/* <div className="cards-container">
-                        <Card style={{ width: '21rem' }}>
-                            <Card.Img className="cards-image" variant="top" src={item.images.jpg.image_url} />
-
-                            <Card.Body className="cards-body">
-                                <Card.Title className="anime-title">{item.name}</Card.Title>
-
-                                {item.genres.map((genre) => (
-                                    <Card.Subtitle className="anime-category" key={genre.id}>{genre.name}</Card.Subtitle>
-                                ))}
-
-                                <Card.Text></Card.Text>
-                                <Button className="view-more-btn" variant="primary">View more</Button>
-                            </Card.Body>
-                        </Card>
-                    </div> */}
-
-
-                    <div className="cards-container">
-                        {animeData.map((animeTitle) => {
-                            
-                            return( <Card style={{ width: '21rem' }}>
-                                        <Card.Img className="cards-image" variant="top" src={animeTitle.images.jpg.image_url} />
-                                        <Card.Body className="cards-body">
-                                            <Card.Title className="anime-title">{animeTitle.title}</Card.Title>
+                <div className="cards-container">
+                    {animeData.map((animeTitle) => {
+                        return( 
+                                <Card style={{ width: '21rem' }}>
+                                    <Card.Img className="cards-image" variant="top" src={animeTitle.images.jpg.image_url} />
+                                        
+                                    <Card.Body className="cards-body">
+                                        <Card.Title className="anime-title"><h3>{animeTitle.title}</h3></Card.Title>
+                                        
+                                        <div className="genre-container">
                                             {animeTitle.genres.map((genre) => {
                                                 return <Card.Subtitle className="anime-genre">{genre.name}</Card.Subtitle>
                                             })}
-                                            {/* <Card.Subtitle className="anime-genre">{}</Card.Subtitle> */}
-                                            <Card.Text></Card.Text>
-                                            <Button className="view-more-btn" variant="primary">View more</Button>
-                                        </Card.Body>
-                                    </Card>
-                            )
-                            })}
-                    </div>
+                                        </div>
+
+                                        <div className="score-container">
+                                            <h2>{animeTitle.score}</h2>
+                                        </div>
+                        
+                                        
+
+                                        {/* <Card.Subtitle className="anime-genre">{}</Card.Subtitle> */}
+                                        <Card.Text></Card.Text>
+                                        <Button className="view-more-btn" variant="primary">View more</Button>
+                                    </Card.Body>
+
+                                </Card>
+                        )
+                        })}
+                </div>
 
             </>
             )
