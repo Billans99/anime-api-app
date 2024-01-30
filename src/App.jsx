68714261c004ea56e3365225d5f2db43d1849894
Billy <billy.xu99@gmail.com>
2024-01-30 17:18:37 +1000

    Finished fetching data from api, displaying anime title, and anime genre complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,23 @@
 // Using Jikan 4.0 API (REST) to fetch data
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import Header from './Header.jsx'
 import Cards from './Cards.jsx'
-
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 
 
 const App = () => {
 
 
-
-
+  
  
 
   return (
     <>
       <Header/>
       <Cards/>
-     
     </>
   )
 }
--- src/Cards.jsx
+++ src/Cards.jsx
@@ -1,79 +1,74 @@
-// importing components from React-bootstrap import React, { useState, useEffect } from 'react';
+// importing components from React-bootstrap import React, { useState, useEffect } from 'react'
 import Button from 'react-bootstrap/Button'
 import Card from 'react-bootstrap/Card'
 import { useState, useEffect } from 'react'
+import React from 'react'
+import axios from 'axios'
 
-
-
-// function Cards() {
-//     const [data, setData] = useState([]);
-
-//     useEffect(() => {
-//         fetch('https://api.jikan.moe/v4/anime?q=&sfw')
-//             .then(response => response.json())
-//             .then(data => setData(data.data));
-//     }, []);
-
-//     return (
-//         <div>
-//             {data.map(item => (
-//                 <div key={item.id}>
-//                     <h2>{item.attributes.titles.en}</h2>
-//                     <img src={item.attributes.posterImage.small} alt={item.attributes.titles.en} />
-//                 </div>
-//             ))}
-//         </div>
-//     );
-// }
 
 
 // This is the main page for the Cards component
 const Cards = () => {
-    const [data, setData] = useState([])
-
-//  Get data from Jikan API URL, then convert to JSON format, then set the data to the state.
-const fetchData = () => { 
-    return fetch('https://api.jikan.moe/v4/anime?q=&sfw') 
-            .then((res) => res.json()) 
-            .then((data) => setData(data)) 
-            console.log('data', data.data)
+    const [animeData, setAnimeData] = useState([])
+    const [url, setUrl] = useState('https://api.jikan.moe/v4/anime?q=&sfw')
+  
+    const getAnimeData = async () => {
+        await axios
+        .get(url)
+        .then(response => {
+            setAnimeData(response.data.data)
+            console.log(animeData)
+        })
     }
-    
+  
     useEffect(() => {
-        fetchData();
+      getAnimeData()
     }, [])
-
-
-
-
-    return(
-        <>
-             {/* iterate the data with map to the state and display it in the cards  */}
-                 {data.map((item) => {(
-                    <div className="cards-container">
+  
+                                    
+    return (
+            <>
+                {/* {data.map((item) => ( */}
+                    {/* <div className="cards-container">
                         <Card style={{ width: '21rem' }}>
-                            <Card.Img className="cards-image" variant="top" src={item.images.jpg.image_url}/>
+                            <Card.Img className="cards-image" variant="top" src={item.images.jpg.image_url} />
+
                             <Card.Body className="cards-body">
                                 <Card.Title className="anime-title">{item.name}</Card.Title>
-                                {item.genres.map((genre) => {
-                                <Card.Subtitle className="anime-category">{item.genres.genre.name}</Card.Subtitle>
-                                })}
-                                {/* Card text if needed  */}
+
+                                {item.genres.map((genre) => (
+                                    <Card.Subtitle className="anime-category" key={genre.id}>{genre.name}</Card.Subtitle>
+                                ))}
+
                                 <Card.Text></Card.Text>
-    
                                 <Button className="view-more-btn" variant="primary">View more</Button>
                             </Card.Body>
                         </Card>
+                    </div> */}
+
+
+                    <div className="cards-container">
+                        {animeData.map((animeTitle) => {
+                            
+                            return( <Card style={{ width: '21rem' }}>
+                                        {/* <Card.Img className="cards-image" variant="top" src={} /> */}
+                                        <Card.Body className="cards-body">
+                                            <Card.Title className="anime-title">{animeTitle.title}</Card.Title>
+                                            {animeTitle.genres.map((genre) => {
+                                                return <Card.Subtitle className="anime-genre">{genre.name}</Card.Subtitle>
+                                            })}
+                                            {/* <Card.Subtitle className="anime-genre">{}</Card.Subtitle> */}
+                                            <Card.Text></Card.Text>
+                                            <Button className="view-more-btn" variant="primary">View more</Button>
+                                        </Card.Body>
+                                    </Card>
+                            )
+                            })}
                     </div>
-                  )})}
-                
 
-                
+            </>
+            )
+        } 
 
-     
-            
-        </>
-    )
-}
 
 export default Cards
