521a0a539a63cce0aadddadd0c2c6fefb268b2e6
Billy <billy.xu99@gmail.com>
2024-02-03 18:04:59 +1000

    Added comments at top, of what to do next

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
+
+// TO DO LIST: 
+
+// - Add a 'MoreInfo' Modal component onClick of "view-more" button 
+// - Create styling for the MoreInfo component OR delete MoreInfo component if not necessary
+// - Delete genre section of cards to create more space for the title and image
+// - Increase height to Cards.jsx images to make them appear more clear
+// - Complete logic for the search feature
+// - Do upcoming and popular animes list
+
+
+
+
+
 // Using Jikan 4.0 API (REST) to fetch data
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Header from './Header.jsx'
 import Cards from './Cards.jsx'
+import MoreInfo from './MoreInfo.jsx'
 import { useState, useEffect } from 'react'
-import axios from 'axios'
 
 
 
@@ -18,6 +32,7 @@ const App = () => {
     <>
       <Header/>
       <Cards/>
+      <MoreInfo/>
     </>
   )
 }
