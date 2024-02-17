33510cab72a7d1c7246a0099e498b767cdff6bb0
Billy <billy.xu99@gmail.com>
2024-02-17 21:08:02 +1000

    Added additional information of things to do next

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,9 @@
 // TO DO LIST: 
 
 
-
-// - 1. Add characters to the bottom of view-more-modals. (`https://api.jikan.moe/v4/anime/{id}/characters`)
-//        Get the mal_id of whatever anime is clicked on, store that id (num) in a state variable (selectedAnimeID)
-//        when view-more btn is clicked, get character data from api with that new endpoint 
-//        conditionally render characters if it is not null, it is looking for a number, so if it is null, it will not render
-
-// - 3. Add trailers to the bottom of view-more-modals.
-// - 4. Flesh out app and add as much data as possible to the view-more-modals and anime-app to keep learning more
+// - 1. Add trailers of view-more-modals above characters data.
+// - 2. Add logic for images to the cards component and format to look nice
+// - 3. Flesh out app and add as much data as possible to the view-more-modals and anime-app to keep learning more
 
 
 
