734b776e985dd46284b90f109e92b2129ddf2bad
Billy <billy.xu99@gmail.com>
2024-03-04 20:49:59 +1000

    Removed completed items from to-do list and added more planned info/data to add to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
 
 // TO DO LIST: 
 
-// - 1. Add progress bar for stats-tab later
 
 
-// - 2. See if theres a way to load some of the character and reviews data more efficiently, sometimes it doesn't load fast enough and shows previous characters and reviews data
-// - 3. Makes more comments to explain code
-// - 4. Flesh out app and add as much data as possible to the view-more-modals and anime-app to keep learning more
+// - 1. Flesh out app and add as much data as possible to the view-more-modals and anime-app to keep learning more
+//        Add recommendations tab to app (Jikan Recommendations section)
+//        Add related anime tab to app (Jikan Relations section)
+//        Add theme music tab to app (Jikan themes section)
+//        Add external information tab to app (Jikan External section)
+//        Add Streaming tab to app (Jikan streaming section)
+//        Add voice actors tab to app (Jikan Character voice actors section)
+
+//        Add random btn, top anime, and popular anime, recommendations (these are different to anime specific recommendations 
+//           as these are random recommendations) to the header
+
+
+// - 2. Makes more comments to explain code
+// - 3. See if theres a way to load some of the character and reviews data more efficiently, sometimes it doesn't load fast enough and shows previous characters and reviews data
 // - Add stats, recommendations, pictures, suggested order of viewing (More info section @ MAL website),
 // - voice actors, staff names and photos, 
-// - 5. Add top anime and popular anime to the app
-// - 6. Add manga to the app
+// - 4. Add top anime and popular anime to the app
+// - 5. Add manga to the app
 
 
 
